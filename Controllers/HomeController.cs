1ac9e1e39519c0803ff2b4a68e1043bafe0697d5
MahmoudMahdi3 <mhmdmhd073@gmail.com>
2024-12-08 18:03:07 +0200

    Admin

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication7.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Mailbox()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            ViewBag.Message = "Fill the form.";
+
+            return View();
+        }
+
+        public ActionResult compose()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+    }
+}
